7276060f4d5fc6d32daa71552495afdefbe7923b
Subrat <logosubrat@gmail.com>
2022-06-11 01:03:11 -0400

    Factorial

---
+++ src/problems/math/FindLowestDifference.java
@@ -0,0 +1,26 @@
+package problems.math;
+
+import java.util.*;
+
+public class FindLowestDifference {
+
+
+    /*
+     *
+     * consider these below two arrays (array1 and array2)
+     * find the lowest difference between the two arrays cells
+     * hint: the lowest difference between cells is 1
+     *
+     */
+
+    public static void main(String[] args) {
+
+
+        double x = 30;
+        double y = 18;
+
+        //int m = array1.length;
+        //int n = array2.length;
+        System.out.println("Maximum number of array1 and array2 is: " +Math.max(x,y));
+    }
+}
